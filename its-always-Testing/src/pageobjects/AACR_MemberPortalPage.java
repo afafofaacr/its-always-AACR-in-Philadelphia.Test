0482f831aac53d7eec602c34d6c31f49aa42f4ca
lperry <53439922+lperry@users.noreply.github.com>
2020-01-21 16:41:42 -0500

    Renew via Portal
    Active and Affiliate works, can tell what kind of membership it is by looking at the portal's badge name.

--- its-always-Testing/src/pageobjects/AACR_MemberPortalPage.java
+++ its-always-Testing/src/pageobjects/AACR_MemberPortalPage.java
@@ -32,5 +32,31 @@ public class AACR_MemberPortalPage {
 	@TextType()
 	@FindBy(xpath = "//div[@id='myMembership']/div/div/div/div/div[normalize-space(.)='Your application has been submitted and is awaiting approval. Please check back soon or contact membership at membership@aacr.org.']")
 	public WebElement myMembershipMessageNeedsApproval;
-			
+	@TextType()
+	@FindBy(xpath = "//article//div/img")
+	public WebElement membershipBadge;
+	@ButtonType()
+	@FindByLabel(label = "Launch Directory")
+	public WebElement launchDirectory;
+	
+	public int membershipType(String str){
+	
+		if(str.contains("Active")){
+			return 1;
+		}
+		else if(str.contains("Affiliate")){
+			return 2;
+		}
+		else if(str.contains("Associate")){
+			return 3;
+		}		
+		return 4;
+	}
+
+	@ButtonType()
+	@FindByLabel(label = "Current Year")
+	public WebElement currentYear;
+	@ButtonType()
+	@FindByLabel(label = "Next")
+	public WebElement next;
 }
